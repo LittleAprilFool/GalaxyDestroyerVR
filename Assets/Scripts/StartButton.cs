0d57b7e58f678f35b0e71b20534539b96ab8e64a
AprilWong <aprilwang.zju@gmail.com>
2015-03-21 09:38:56 -0700

    ready for button

---
+++ Assets/Scripts/StartButton.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartButton : MonoBehaviour {
+
+	private CardboardHead head;
+	// Use this for initialization
+	
+	void Start () {
+		head = Camera.main.GetComponent<StereoController>().Head;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RaycastHit hit;
+		bool isLookedAt = GetComponent<Collider>().Raycast(head.Gaze, out hit, Mathf.Infinity);
+
+		if (Cardboard.SDK.CardboardTriggered && isLookedAt) {
+			
+		}
+	}
+}
