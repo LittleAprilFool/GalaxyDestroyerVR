aaca735d8c498007bdc97ca8e138730262df3eae
AprilWong <aprilwang.zju@gmail.com>
2015-03-21 14:27:19 -0700

    almost

---
+++ Assets/Scripts/IntroText.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IntroText : MonoBehaviour {
+	public int timer;
+	private GameObject text;
+	// Use this for initialization
+	void Start () {
+		text = GameObject.Find("Introduction");
+		text.SetActive (true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (timer != 0)
+			timer--;
+		if (timer == 0)
+			text.SetActive (false);
+	}
+}
--- Assets/Scripts/StartButton.cs
+++ Assets/Scripts/StartButton.cs
@@ -1,22 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
-public class StartButton : MonoBehaviour {
-
+[RequireComponent(typeof(Collider))]
+	public class StartButton : MonoBehaviour {
 	private CardboardHead head;
-	// Use this for initialization
-	
-	void Start () {
+	private Vector3 startingPosition;
+	public GameObject ButtonTexture;
+	private int timer;
+	void Start() {
 		head = Camera.main.GetComponent<StereoController>().Head;
+		startingPosition = transform.localPosition;
+		ButtonTexture = GameObject.Find("Texture");
+		timer = 0;
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	void Update() {
 		RaycastHit hit;
 		bool isLookedAt = GetComponent<Collider>().Raycast(head.Gaze, out hit, Mathf.Infinity);
 
-		if (Cardboard.SDK.CardboardTriggered && isLookedAt) {
-			
+		if (isLookedAt) {
+			timer ++;
+			Color blue = new Vector4 (0.3f, 1f, 0.8f, 1f);
+			Debug.Log (blue);
+			ButtonTexture.GetComponent<RawImage> ().color = blue;
+			if (timer == 100) {
+				Application.LoadLevel(1);
+			}
+		} else {
+			ButtonTexture.GetComponent<RawImage> ().color = Color.white;
+			timer = 0;
+		}
+
+		if (Cardboard.SDK.CardboardTriggered) {
+			Application.LoadLevel(1);
 		}
 	}
 }
