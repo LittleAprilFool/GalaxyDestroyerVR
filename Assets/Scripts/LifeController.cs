62d8f1a4d856150e31c555dbc068daf189a34cf5
AprilWong <aprilwang.zju@gmail.com>
2015-03-21 01:59:44 -0700

    finish status bar

--- Assets/Scripts/LifeController.cs
+++ Assets/Scripts/LifeController.cs
@@ -1,13 +1,23 @@
-﻿using UnityEngine;
+﻿// README
+// 
+// after been attacked
+// public void loselife()
+
+using UnityEngine;
 using System.Collections;
 
 public class LifeController : MonoBehaviour {
 	public int lifenumber;
 	public float delta;
 
+//	void Update(){
+//		if (Input.GetMouseButtonDown(1))
+//			loselife();
+//	}
+
 	public void loselife(){
 		if (lifenumber > 0) {
-			transform.localScale += new Vector3 (0, delta, 0);
+			transform.localScale += new Vector3 (delta, 0, 0);
 			lifenumber --;
 			if(lifenumber == 0) Application.LoadLevel(1);
 		}
